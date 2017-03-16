9c7837fa8c9743527fb42fec32fd40680c0bb531
awo-dev <!9HdGbpfM?>
2017-03-16 16:06:58 +0100

    basic commands works

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import WeightClient.WeightClient;
+
 /**
  * Created by awo on 16/03/17.
  */
 public class Main {
     public static void main(String[] args) {
-
+        WeightClient wc = new WeightClient();
+        try {
+            wc.connect("localhost", 8000);
+            System.out.println("Weight is currently " + wc.getCurrentWeight() + "!");
+            wc.writeToPrimaryDisplay("Idiot!");
+        } catch (Exception e) {
+            System.err.println("FEJL!!! : " + e.getMessage());
+        }
     }
 }
--- src/WeightClient/WeightClient.java
+++ src/WeightClient/WeightClient.java
@@ -7,14 +7,50 @@ package WeightClient;
 import TCP.TCPClient;
 import TCP.ITCPClient;
 
+import java.io.IOException;
+
 public class WeightClient {
     private ITCPClient tcp;
     private String host;
     private int port = 8000; // Default port
 
-    public WeightClient(String host, int port) {
+    public WeightClient() {
         this.tcp = new TCPClient();
+    }
+
+    public void connect(String host, int port) throws Exception {
         this.host = host;
         this.port = port;
+
+        try {
+            tcp.connect(host, port);
+        } catch (IOException e) {
+            throw new Exception("Failed to connect to " + host + " on port " + port + ": " + e.getMessage());
+        }
+    }
+
+    // S command
+    public double getCurrentWeight() throws Exception {
+        // S S      0.900 kg
+        String receivedMessage = "";
+        try {
+            tcp.send("S");
+            receivedMessage = tcp.receive();
+        } catch (IOException e) {
+            throw new Exception("Failed to retrieve the current weight: " + e.getMessage());
+        }
+        receivedMessage = receivedMessage.replace(" kg", "");
+        return Double.parseDouble(receivedMessage.substring(9));
+    }
+
+    // D command
+    public void writeToPrimaryDisplay(String message) throws Exception {
+        try {
+            tcp.send("D \"" + message + "\"");
+            if (!tcp.receive().equals("D A"))
+                throw new Exception("Something went wrong when displaying message on the weight.");
+        } catch (IOException e) {
+            throw new Exception("Failed to send the message: " + e.getMessage());
+        }
     }
 }
