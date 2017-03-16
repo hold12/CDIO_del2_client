55e06f9c5c69883af217275436722c3c26b6ccb0
awo-dev <!9HdGbpfM?>
2017-03-16 15:02:55 +0100

    Added basic main and basic unit test

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by awo on 16/03/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ test/MainTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by awo on 16/03/17.
+ */
+public class MainTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void main(String[] args) throws Exception {
+        assertTrue(!false);
+    }
+
+}
